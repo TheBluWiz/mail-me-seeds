ad639a6a72216a65b68f76f5cddad72a38a636d4
Jamey Wicklund <theBluWiz@icloud.com>
2023-01-14 09:52:36 -0800

    Resolves merge conflicts

--- controllers /api/index.js
+++ controllers /api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes"); //logon logout sign up
 const offerRoutes = require("./offerRoutes");
+const passwordReset = require('./reset');
 
 router.use("/users", userRoutes);
 router.use("/offers", offerRoutes);
+router.use('/passwordReset', passwordReset)
 
 module.exports = router;
--- controllers/api/index.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-const passwordReset = require('./reset');
-
-router.use('/passwordReset', passwordReset)
