95d0172e9c323b770a45dcbd485665a086be0150
Jamey Wicklund <theBluWiz@icloud.com>
2023-01-18 09:17:39 -0800

    Adds consistency through DB, Server, and .env
    The server can now run.

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
 //these are the things we need for the program to run //these are file paths
-const apiRoutes = require("./api");
+// const apiRoutes = require("./api");
 const offerRoutes = require("./offers");
-const userRoutes = require("./user");
+const userRoutes = require("./users");
 
 //this is where our program actually happens //These paths are actual URL paths
-router.use("/api", apiRoutes);
+// router.use("/api", apiRoutes);
 router.use("/offers", offerRoutes); //using the default localhost 3001
 router.use("/user", userRoutes);
 
