84a8e7cff29f7709572dba5c77f08b4702e33435
Rhys Smoker <r.smoker@gmail.com>
2023-01-17 22:00:13 -0800

    added seed offer routes

--- controllers/offers.js
+++ controllers/offers.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 
 //localhost3001:/offers/
-router.use("");
+
+router.get("/request", async (req, res) => {
+	res.render("request");
+});
+
+router.get("/myoffers", async (req, res) => {
+	res.render("myoffers");
+});
+
+router.get("/form", async (req, res) => {
+	res.render("offerform");
+});
+
+// router.get("/comments", async (req, res) => {
+//     res.render("comment");
+// })
 
 module.exports = router;
