751ce9648b0b51879d25fb464e2732719eb2dad2
Jamey Wicklund <theBluWiz@icloud.com>
2023-01-18 17:22:45 -0800

    Creates /api/mailService branch which will be used for all mailing functions.
    Moved resetPassword functions into /api/mailService

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userActions = require("./userActions");
+const mailService = require("./mailService")
 
 router.use("/userActions", userActions);
+router.use("/mailService", mailService);
 
 module.exports = router;
