8011edb79903b29097bdaf73298b2aa5de9dfd53
Jamey Wicklund <theBluWiz@icloud.com>
2023-01-18 16:18:49 -0800

    Adds EmailOffer model

---
+++ models/EmailOffer.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class EmailOffer extends Model {}
+
+EmailOffer.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    resetLink: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: { // who offered the seeds
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'emailReset',
+  }
+);
+
+module.exports = EmailOffer;
