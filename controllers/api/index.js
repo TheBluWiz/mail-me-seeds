1287249de98671271da44d6539b387df2d9dc67b
Jamey Wicklund <theBluWiz@icloud.com>
2023-01-19 16:35:00 -0800

    merged dev

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userActions = require("./userActions");
 const mailService = require("./mailService")
+const offerUpdates = require("./offerUpdates")
 
 router.use("/userActions", userActions);
 router.use("/mailService", mailService);
+router.use("/offerUpdates", offerUpdates);
 
 module.exports = router;
