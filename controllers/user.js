b0c925589432ed1e16f311e91f88fd7279569f4b
Jamey Wicklund <theBluWiz@icloud.com>
2023-01-22 11:51:32 -0800

    Fixes log in/log out button on Account page

--- controllers/user.js
+++ controllers/user.js
@@ -65,7 +65,7 @@ router.get("/updatepassword/:resetLink", async (req, res) => {
 });
 
 router.get("/account", withAuth, async (req, res) => {
-	res.render("account");
+  res.render("account", { data });
 });
 
 module.exports = router;
